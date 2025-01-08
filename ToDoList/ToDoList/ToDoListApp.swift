851f3579a32a5d300dc77bdf1e67fd5e69e96668
Chingis <Kekwaker@gmail.com>
2025-01-08 13:25:36 +0800

    my first commit in 2025

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Qin Chingis on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
